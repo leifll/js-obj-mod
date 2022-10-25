be72f54eb59678e3d9d0c47792ec53c0dc171b9b
Leif <leifl@kth.se>
2022-10-25 15:52:30 +0200

    created JS project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Mina föreläsningsanteckningar finns i ~/pCloudDrive/BACKUPS_DONT_TOUCH/id1354-bup190213-final2018/lectures/js-language/js-language.pdf.
+
+function Person(firstName, age) {
+  const self = this;
+  self.firstName = firstName;
+  self.age = age;
+  }
+  
+  const myMother = new Person("Fia", 48);
