a63f2c22d21b71f2e2e51b16f8d17987ce9e0082
Leif <leifl@kth.se>
2022-10-27 16:22:56 +0200

    proto checks logged

--- src/index.js
+++ src/index.js
@@ -1,9 +1,53 @@
-// Mina föreläsningsanteckningar finns i ~/pCloudDrive/BACKUPS_DONT_TOUCH/id1354-bup190213-final2018/lectures/js-language/js-language.pdf.
+'use strict';
 
+const util = require('util');
+
+/* Mina föreläsningsanteckningar finns i ~/pCloudDrive/BACKUPS_DONT_TOUCH/
+id1354-bup190213-final2018/lectures/js-language/js-language.pdf.*/
+
+/**
+ * Constructor function for a Person object.
+ * @param {string} firstName - The first name of the newly constructed Person object.
+ * @param {number} age - The age of the newly constructed Person object.
+ */
 function Person(firstName, age) {
-  const self = this;
-  self.firstName = firstName;
-  self.age = age;
+  firstName = firstName;
+  age = age;
+};
+
+console.log(Person.getPrototypeOf);
+console.log(Object.getPrototypeOf);
+console.log(Object.getPrototypeOf());
+
+const myMother = new Person('Fia', 48);
+
+const printSeparator = () => console.log('---------------------------------'); 
+
+const logPrototype = (name, obj) => {
+  if (obj === undefined || Object.getPrototypeOf(obj) === undefined) {
+    return;
   }
-  
-  const myMother = new Person("Fia", 48);
+  console.log('Object.getPrototypeOf(' + name + ')):' + Object.getPrototypeOf(obj));
+  logPrototype('Object.getPrototypeOf(' + name + ')' + Object.getPrototypeOf(obj));
+};
+
+const logObj = (name, obj) => {
+  console.log(name + ':' + obj); // calls obj.toString()
+  console.log(name + '.constructor:' + obj.constructor);
+  logPrototype(name, obj);
+  printSeparator();
+};
+
+logObj('myMother', myMother);
+logObj('Person', Person);
+logObj('Object', Object);
+
+console.log(util.inspect(Person, {showHidden: true, depth: null, colors: true}));
+printSeparator();
+console.log(util.inspect(Object.getPrototypeOf(Person),
+    {showHidden: true, depth: null, colors: true}));
+printSeparator();
+console.log(util.inspect(Object, {showHidden: true, depth: null, colors: true}));
+printSeparator();
+console.log(util.inspect(Object.getPrototypeOf(Object),
+    {showHidden: true, depth: null, colors: true}));
